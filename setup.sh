fae7c45b0471d59d1002c608bfbc784790f59f51
Cole Robinette <robinette.31@gmail.com>
2019-02-14 11:01:33 -0500

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-sudo pip install git+https://github.com/excederstrand/exchangelib.git
+sudo -H -E pip install git+ssh://git@github.com/ecederstrand/exchangelib.git
 git update-index --assume-unchanged credentials
